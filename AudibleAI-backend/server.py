d37def29fb36b04645ff74233cddccdccee61cff
nkraj7266 <nkraj7266@gmail.com>
2025-09-04 08:21:28 +0530

    [add] added logging in db queries and server code in backend

--- AudibleAI-backend/server.py
+++ AudibleAI-backend/server.py
@@ -41,8 +41,7 @@ with app.app_context():
 # Flask error handler
 @app.errorhandler(Exception)
 def handle_exception(e):
-    app_logger.error(f"Exception: {e}", exc_info=True)
-    error_logger.error(f"Exception: {e}", exc_info=True)
+    error_logger.error(f"Unhandled Exception: {e}", exc_info=True)
     return {"error": str(e)}, 500
 
 if __name__ == '__main__':
